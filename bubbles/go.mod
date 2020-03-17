87b9f5f7cf06118d7cdb4fb0b1cdc93c7e85c3ea
Marcus Dantas <mpsdantas15@gmail.com>
2020-03-17 11:40:12 -0300

    Updating main to use new cvutils version

--- bubbles/go.mod
+++ bubbles/go.mod
@@ -3,6 +3,6 @@ module bubbles
 go 1.13
 
 require (
-	github.com/mpsdantas/cvutils v1.0.0
+	github.com/mpsdantas/cvutils v1.1.0
 	gocv.io/x/gocv v0.22.0
 )
--- bubbles/main.go
+++ bubbles/main.go
@@ -1,87 +1,60 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/mpsdantas/cvutils"
 	"gocv.io/x/gocv"
 )
 
-type ImageOptions struct {
-	Name       string
-	Flags      gocv.IMReadFlag
-	WindowName string
-	Draw       DrawOptions
-}
-
-type DrawOptions struct {
-	Color  cvutils.Color
-	StartX int
-	EndX   int
-	StartY int
-	EndY   int
-}
-
-func ShowIMG(opts ImageOptions) {
-	img := cvutils.Image{
-		Mat: gocv.IMRead(opts.Name, opts.Flags),
-	}
-
-	if img.Mat.Empty() {
-		log.Fatal(fmt.Sprintf("Error reading image from: %v\n", opts.Name))
-	}
-
-	defer img.Mat.Close()
-
-	window := gocv.NewWindow(opts.WindowName)
-
-	for i := opts.Draw.StartX; i < opts.Draw.EndX; i++ {
-		for j := opts.Draw.StartY; j < opts.Draw.EndY; j++ {
-			img.SetColor(i, j, opts.Draw.Color)
-		}
-	}
-
-	window.IMShow(img.Mat)
-	window.WaitKey(0)
+func Draw(image cvutils.Image, color cvutils.Color, point cvutils.Point) {
+	image.SetColor(point.X, point.Y, color)
 }
 
 func main() {
 	name := "bolhas.png"
 
 	// Show image with black trace
-	ShowIMG(ImageOptions{
+	cvutils.ShowIMG(cvutils.ImageOptions{
 		Name:       name,
 		Flags:      gocv.IMReadGrayScale,
 		WindowName: "Bubbles black and white",
-		Draw: DrawOptions{
+		Draw: cvutils.DrawOptions{
+			DrawFunc: Draw,
 			Color: cvutils.Color{
 				Red:   0,
 				Green: 0,
 				Blue:  0,
 			},
-			StartX: 200,
-			EndX:   210,
-			StartY: 10,
-			EndY:   200,
+			StartingPoint: cvutils.Point{
+				X: 200,
+				Y: 10,
+			},
+			EndPoint: cvutils.Point{
+				X: 210,
+				Y: 200,
+			},
 		},
 	})
 
 	// Show image with red trace
-	ShowIMG(ImageOptions{
+	cvutils.ShowIMG(cvutils.ImageOptions{
 		Name:       name,
 		Flags:      gocv.IMReadColor,
 		WindowName: "Bubbles black and white",
-		Draw: DrawOptions{
+		Draw: cvutils.DrawOptions{
+			DrawFunc: Draw,
 			Color: cvutils.Color{
 				Red:   255,
 				Green: 0,
 				Blue:  0,
 			},
-			StartX: 200,
-			EndX:   210,
-			StartY: 10,
-			EndY:   200,
+			StartingPoint: cvutils.Point{
+				X: 200,
+				Y: 10,
+			},
+			EndPoint: cvutils.Point{
+				X: 210,
+				Y: 200,
+			},
 		},
 	})
 }
